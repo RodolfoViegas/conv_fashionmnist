de241d1c86a511bb7b8fc117e6569515af5f78c3
Rodolfo Viegas de Albuquerque <35511446+RodolfoViegas@users.noreply.github.com>
2020-06-25 21:56:11 -0300

    Update conv_fashion_MNIST.py

--- conv_fashion_MNIST.py
+++ conv_fashion_MNIST.py
@@ -34,10 +34,9 @@ test_images=test_images/255.0
 
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
   tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2, 2),
+  
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dense(10, activation='softmax')
